076b5420b0fef40b9e0cf4d508344af5b18a7367
Maksim Kostromin <daggerok@gmail.com>
2017-10-23 10:38:52 +0300

    update spring-boot version

--- src/test/java/com/daggerok/spring/ssl/SslApplicationTests.java
+++ src/test/java/com/daggerok/spring/ssl/SslApplicationTests.java
@@ -2,15 +2,13 @@ package com.daggerok.spring.ssl;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@WebAppConfiguration
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SslApplication.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SslApplicationTests {
 
-    @Test
-    public void contextLoads() {}
+  @Test
+  public void contextLoads() {}
 }
